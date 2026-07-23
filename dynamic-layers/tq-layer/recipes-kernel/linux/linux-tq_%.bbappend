quitschbo/meta-gyroidos-nxp

commit 12bf169f1f97c7808ce1ccc8dd957d7c9f0e4063

dynamic-layers/tq-layer: linux-tq: remove patch for FDB in userns
This patch was merged in 6.12 lts branch upstream. Lately, tq updated
the branch in its kernel fork, thus containing this path now, too.
Thus, we have to drop this in our layer.

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

dynamic-layers/tq-layer/recipes-kernel/linux/linux-tq_%.bbappend
@@ -1,6 +1,0 @@
-require recipes-kernel/linux/linux-gyroidos.inc
-
-FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
-
-IS_VERSION_612 := "${@oe.utils.trim_version(d.getVar('PV'), 2) == '6.12'}"
-SRC_URI += "${@oe.utils.vartrue('IS_VERSION_612', 'file://0001-rtnetlink-Allow-deleting-FDB-entries-in-user-namespa.patch', '', d)}"

dynamic-layers/tq-layer/recipes-kernel/linux/linux-tq/0001-rtnetlink-Allow-deleting-FDB-entries-in-user-namespa.patch
@@ -1,55 +1,0 @@
-From 87593d379966e40470a7814f70145db947d8044f Mon Sep 17 00:00:00 2001
-From: =?UTF-8?q?Johannes=20Wiesb=C3=B6ck?=
- <johannes.wiesboeck@aisec.fraunhofer.de>
-Date: Tue, 26 Aug 2025 19:05:21 +0200
-Subject: [PATCH net v2] rtnetlink: Allow deleting FDB entries in user
- namespace
-MIME-Version: 1.0
-Content-Type: text/plain; charset=UTF-8
-Content-Transfer-Encoding: 8bit
-
-Creating FDB entries is possible from a non-initial user namespace when
-having CAP_NET_ADMIN, yet, when deleting FDB entries, processes receive
-an EPERM because the capability is always checked against the initial
-user namespace. This restricts the FDB management from unprivileged
-containers.
-
-Drop the netlink_capable check in rtnl_fdb_del as it was originally
-dropped in c5c351088ae7 and reintroduced in 1690be63a27b without
-intention.
-
-This patch was tested using a container on GyroidOS, where it was
-possible to delete FDB entries from an unprivileged user namespace and
-private network namespace.
-
-Fixes: 1690be63a27b ("bridge: Add vlan support to static neighbors")
-Reviewed-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>
-Tested-by: Harshal Gohel <hg@simonwunderlich.de>
-Signed-off-by: Johannes Wiesböck <johannes.wiesboeck@aisec.fraunhofer.de>
----
-v2:
-  - completely drop the capability check in favor of changing to
-    netlink_net_capable
-  - describe intended behavior already possible for adding FDB entries
-v1: https://lore.kernel.org/all/20250923082153.60030-1-johannes.wiesboeck@aisec.fraunhofer.de/
----
- net/core/rtnetlink.c | 3 ---
- 1 file changed, 3 deletions(-)
-
-diff --git a/net/core/rtnetlink.c b/net/core/rtnetlink.c
-index 094b085cff206..8f3fd52f089d2 100644
---- a/net/core/rtnetlink.c
-+++ b/net/core/rtnetlink.c
-@@ -4707,9 +4707,6 @@ static int rtnl_fdb_del(struct sk_buff *skb, struct nlmsghdr *nlh,
- 	int err;
- 	u16 vid;
- 
--	if (!netlink_capable(skb, CAP_NET_ADMIN))
--		return -EPERM;
--
- 	if (!del_bulk) {
- 		err = nlmsg_parse_deprecated(nlh, sizeof(*ndm), tb, NDA_MAX,
- 					     NULL, extack);
--- 
-2.51.0
-

commit 4b4f3100eee70133e78591f6f3eb62d70e342765

dynamic-layers/tq-layer: linux-tq: add dependency linux-gyroidos.inc
Commit 12bf169f1f97 accidentially removed the dependency to
linux-gyroidos.inc. Re-add this to provide propper kernel signing and
config from meta-gyroidos.

Fixes: 12bf169f1f97 ("dynamic-layers/tq-layer: linux-tq: remove patch for FDB in userns")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

dynamic-layers/tq-layer/recipes-kernel/linux/linux-tq_%.bbappend
@@ -1,0 +1,1 @@
+require recipes-kernel/linux/linux-gyroidos.inc